dac17219911673869daa27976dbecd456dcf7645
nekromenzer <bamaneeshagimshan1999@gmail.com>
2025-01-14 23:35:10 +0530

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import ScrollCarousel from "./scroll-carousel";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <ScrollCarousel />;
 }
 
 export default App;
---
+++ src/scroll-carousel/index.jsx
@@ -0,0 +1,5 @@
+const ScrollCarousel = () => {
+  return <div className="bg-red-400 mt-5">ScrollCarousel</div>;
+};
+
+export default ScrollCarousel;
